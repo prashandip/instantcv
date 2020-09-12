aeda534644c06f25057f718f92c0890997cb44e2
Prashandip Lunghimba <prashandip2@gmail.com>
2020-09-12 12:38:22 +0545

    Updates

---
+++ precache-manifest.e6fe40ddf10908b5bfa423c8ae08ee13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a572c00ffb2a0ae61770e41e00bb4f",
+    "url": "/instantcv/index.html"
+  },
+  {
+    "revision": "4d9423a536e149348c17",
+    "url": "/instantcv/static/css/main.186193a9.chunk.css"
+  },
+  {
+    "revision": "63eff0e59af3780621f0",
+    "url": "/instantcv/static/js/2.3edfb2cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/instantcv/static/js/2.3edfb2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9423a536e149348c17",
+    "url": "/instantcv/static/js/main.04c68f12.chunk.js"
+  },
+  {
+    "revision": "1ff5ad196b13b017b712",
+    "url": "/instantcv/static/js/runtime-main.e440d27b.js"
+  }
+]);
